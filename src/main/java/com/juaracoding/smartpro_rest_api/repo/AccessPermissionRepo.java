724efddf821de61c46f76546da62a815f8e23c02
Reynaldi119 <80832429+Reynaldi119@users.noreply.github.com>
2025-05-26 17:56:25 +0700

    Repo Done-1

--- src/main/java/com/juaracoding/smartpro_rest_api/repo/AccessPermissionRepo.java
+++ src/main/java/com/juaracoding/smartpro_rest_api/repo/AccessPermissionRepo.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccessPermissionRepo extends JpaRepository<AccessPermission, Long> {
+
     public Page<AccessPermission> findByNamaContainsIgnoreCase(String nama, Pageable pageable);
     public Page<AccessPermission> findByDeskripsiContainsIgnoreCase(String nama, Pageable pageable);
 
