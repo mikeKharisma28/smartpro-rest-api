ca2bfc8d2fca2bb746c46651e8dc10da6c7d5fdf
Reynaldi119 <80832429+Reynaldi119@users.noreply.github.com>
2025-05-26 14:32:41 +0700

    Repo Done

--- src/main/java/com/juaracoding/smartpro_rest_api/repo/DivisionRepo.java
+++ src/main/java/com/juaracoding/smartpro_rest_api/repo/DivisionRepo.java
@@ -1,7 +1,16 @@
 package com.juaracoding.smartpro_rest_api.repo;
 
 import com.juaracoding.smartpro_rest_api.model.Division;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DivisionRepo extends JpaRepository<Division, Long> {
+    public Page<Division> findByNamaContainsIgnoreCase(String nama, Pageable pageable);
+    public Page<Division> findByDeskripsiContainsIgnoreCase(String nama, Pageable pageable);
+
+    public List<Division> findByNamaContainsIgnoreCase(String nama);
+    public List<Division> findByDeskripsiContainsIgnoreCase(String nama);
 }
