b0f25080c126d8421ca6659559cf6b51610c98fe
Michael Laksa Kharisma <mike_charism@outlook.com>
2025-05-28 17:54:45 +0700

    Updated EditStaffDTO.java

--- src/main/java/com/juaracoding/smartpro_rest_api/dto/validation/EditStaffDTO.java
+++ src/main/java/com/juaracoding/smartpro_rest_api/dto/validation/EditStaffDTO.java
@@ -11,12 +11,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public class EditStaffDTO {
-    @NotNull(message = "Full name tidak boleh kosong")
-    @NotEmpty(message = "Full name tidak boleh kosong")
-    @NotBlank(message = "Full name tidak boleh kosong")
-    @JsonProperty("full-name")
-    private String fullName;
-
     @Pattern(regexp = "^(62|\\+62|0)8[0-9]{9,13}$",
             message = "Invalid phone number format! Allowed format: min 9 max 13 digits after number 8, for example: (0/62/+62)81111111")
     @JsonProperty("phone-number")
@@ -30,20 +24,7 @@ public class EditStaffDTO {
             message = "Minimum format allowed: 1 number, 1 lowercase letter, 1 uppercase letter, 1 special characters (_ \"Underscore\", - \"Hyphen\", # \"Hash\", or $ \"Dollar\" or @ \"At\"). Password length allowed 9-16 characters alphanumeric combinations, example: P@ssw0rd123")
     private String password;
 
-    @NotNull(message = "Division ID tidak boleh kosong")
-    @NotEmpty(message = "Division ID tidak boleh kosong")
-    @NotBlank(message = "Division ID tidak boleh kosong")
-    private Long divisionId;
-
     // setters getters
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -66,13 +47,5 @@ public class EditStaffDTO {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public Long getDivisionId() {
-        return divisionId;
-    }
-
-    public void setDivisionId(Long divisionId) {
-        this.divisionId = divisionId;
     }
 }
